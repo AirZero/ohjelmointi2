0638215ccb667e7579b698cd094c098f62d89ff6
airzero <airzero@gmail.com>
2018-12-05 18:32:06 +0200

    classes for fields

---
+++ src/fxKirjarekisteri/src/fxKirjarekisteri/Kirja.java
@@ -0,0 +1,12 @@
+package fxKirjarekisteri;
+
+/**
+ * 
+ * @author Eetu Rantakangas ja Timo Pakarinen
+ * 
+ * Luokalta voi pyytää kirjojen yksittäisten kenttien tietoja.
+ *
+ */
+public class Kirja {
+
+}
---
+++ src/fxKirjarekisteri/src/fxKirjarekisteri/Kirjat.java
@@ -0,0 +1,14 @@
+package fxKirjarekisteri;
+
+/**
+ * 
+ * @author Eetu Rantakangas ja Timo Pakarinen
+ *
+ * Luokka hoitaa kirjojen tietojen tallentamisen ja tietojen lukemisen kannasta.
+ * 
+ * 
+ */
+public class Kirjat {
+	
+
+}
